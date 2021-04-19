906f544ccaa7ac626d9a1f59d98822f5ae8765c8
Yurafobus1 <agafonovypfobus@gmail.com>
2021-04-19 22:57:29 +0500

    Первые тесты

---
+++ UnitTest1/unittest1.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{		
+	TEST_CLASS(defMethods)
+	{
+	public:
+		
+		TEST_METHOD(main1)
+		{
+			
+		}
+
+	};
+}
